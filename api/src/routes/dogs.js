6f4b13fc47fedbb98ae25573a5ef0126363be752
Angel David Lopez Mathez <alopez@MENDOZACIUDAD.MZA>
2022-10-28 11:40:02 -0300

    Rutas al 90%

--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const axios = require('axios')
 const { Raza } = require('../db')
+const {Op} = require('sequelize');
+const db = require('../db');
 
 const router = Router();
 
@@ -8,73 +10,54 @@ const router = Router();
 
 router.get('/', async (req, res, next) => {
   const {name} = req.query
-  if(name){
-try {
-    
-    let razaApis = axios.get('https://api.thedogapi.com/v1/breeds')
-    let razaDB = Raza.findAll()
+  try {
+    let razaApis = await axios.get('https://api.thedogapi.com/v1/breeds')
 
-    Promise.all([
-        razaApis,
-        razaDB
-    ])
-    .then((respuesta) => {
-        const [razaApis, razaDB] = respuesta   
+ 
+    
+    if(!name) {        
+        let razaDB = await Raza.findAll()      
+            let razasdeApi  = razaApis.data.map((info) => {
+                return {
+                    ID: info.id,
+                    Name: info.name,
+                    Height_max: Number(info.height.imperial.split(" - ")[1]),
+                    Height_min: Number(info.height.imperial.split(" - ")[0]),
+                    Weight_max: Number(info.weight.imperial.split(" - ")[1]),
+                    Weight_min: Number(info.weight.imperial.split(" - ")[0]),                
+                    Life_span:  Number(info.life_span.split(" - ")[0]),
+                    img: info.image.url,                
+                }
+            })
+            let razas = [...razaDB, ...razasdeApi]
         
-        let razasdeApi  = razaApis.data.map((info) => {
-            return {
-                ID: info.id,
-                Name: info.name,
-                Height_max: Number(info.height.imperial.split(" - ")[1]),
-                Height_min: Number(info.height.imperial.split(" - ")[0]),
-                Weight_max: Number(info.weight.imperial.split(" - ")[1]),
-                Weight_min: Number(info.weight.imperial.split(" - ")[0]),                
-                Life_span:  Number(info.life_span.split(" - ")[0]),
-                img: info.image.url,                
-            }
-        })
-        let razas = [...razaDB, ...razasdeApi]
+            res.send(razas)
+        }
     
-        res.send(razas)
-    })
-} catch (error) {
+      
+      if(name){
+        
+        let razaDB = await Raza.findAll({
+            where: { Name: { [Op.substring]: `${name}` } },
+          })
+        let dbFiltrado = razaDB.map((info) => info.Name) 
+        let razasdeApi = razaApis.data.map((info) =>  info.name
+        )
+        let apiFiltrado = razasdeApi.filter(n => n.includes(name) === true)
+          console.log(dbFiltrado)
+          let razas = [...dbFiltrado, ...apiFiltrado]
+          if(razas.length === 0) { res.send("No hay razas disponibles")} else {
+            res.send(razas)
+          }
+       
+          
     
-}
-} 
-  if(!name) {
-try {
+    }
     
-    let razaApis = axios.get('https://api.thedogapi.com/v1/breeds')
-    let razaDB = Raza.findAll()
 
-    Promise.all([
-        razaApis,
-        razaDB
-    ])
-    .then((respuesta) => {
-        const [razaApis, razaDB] = respuesta   
-        
-        let razasdeApi  = razaApis.data.map((info) => {
-            return {
-                ID: info.id,
-                Name: info.name,
-                Height_max: Number(info.height.imperial.split(" - ")[1]),
-                Height_min: Number(info.height.imperial.split(" - ")[0]),
-                Weight_max: Number(info.weight.imperial.split(" - ")[1]),
-                Weight_min: Number(info.weight.imperial.split(" - ")[0]),                
-                Life_span:  Number(info.life_span.split(" - ")[0]),
-                img: info.image.url,                
-            }
-        })
-        let razas = [...razaDB, ...razasdeApi]
-    
-        res.send(razas)
-    })
 } catch (error) {
-    
-}
-  }
-   
+    console.log(error)
+}   
 
 })
 

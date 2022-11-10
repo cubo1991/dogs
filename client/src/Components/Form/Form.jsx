456f668cb150aaf00c6730ffa52da9ab14bd1347
cubo1991 <adavidlopezmathez@gmail.com>
2022-11-10 13:56:49 -0300

    Form avanzado

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -1,45 +1,95 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getTemperaments } from '../../Store/Actions'
 
 const Form = () => {
+  let dispatch = useDispatch()
+  React.useEffect(()=>{
+       
+    dispatch(getTemperaments()) 
+  }, []
+    )
+ 
+  let temperamentos = useSelector(state => state.temperamentos)
+
+  let [formRes, setFormRes] = React.useState   ({
+    name: '',
+    Height_Max: '',
+    Height_Min: '',
+    Weight_Max: '',
+    Weight_Min: '',
+    Life_Span: '',
+    Temperaments: []
+}
+)
+const handleInputChange = (e) => {
+  console.log(e.target.name)
+  console.log(e.target.value)
+  if(e.target.name === "Temperaments") {
+    setFormRes({
+      ...formRes,    
+      [e.target.name] : [...formRes.Temperaments, e.target.value]}) 
+  }else {
+  setFormRes({
+    ...formRes,    
+    [e.target.name] : e.target.value}) 
+
+}}
+
+console.log(formRes)
   return (
     <div>
         <div>
         <form>
             <label> Name </label>
             <input
-            name="Name"
-            type="text"            
+            name="name"
+            type="text"
+            value={formRes.name}
+            onChange={handleInputChange}           
             />
             <label> Height Max</label>
             <input 
-            name="Height Max"
-            type="text" />
+            name="Height_Max"
+            type="text" 
+            onChange={handleInputChange}/>
+                  
           <label> Height Min</label>
             <input 
-            name="Height Min"
-            type="text" />
+            name="Height_Min"
+            type="text" 
+            onChange={handleInputChange}/>
+                  
           <label> Weight Max</label>
             <input 
-            name="Weight Max"
-            type="text" />
+            name="Weight_Max"
+            type="text" 
+            onChange={handleInputChange}/>
+                  
           <label> Weight Min</label>
             <input 
-            name="Weight Min"
-            type="text" />
+            name="weight_Min"
+            type="text" 
+            onChange={handleInputChange}/>
+                  
           <label> Life Span</label>
             <input 
-            name="Life Span"
-            type="text"  />
+            name="Life_Span"
+            type="text"  
+            onChange={handleInputChange} />
+                 
             <label>Temperaments</label>
-         <select>
-         <option value="grapefruit">Grapefruit</option>
-  <option value="lime">Lime</option>
-  <option selected value="coconut">Coconut</option>
-  <option value="mango">Mango</option>
+         <select onChange={handleInputChange} name="Temperaments">
+        
+        {temperamentos.map((t) =>{
+          return(
+            <option value={t.NameT}>{t.NameT}</option>
+          )
+        })}
          </select>
     
         </form>
-        <input type="submit" value="Enviar" />
+        <input type="submit"  value="Enviar" />
         </div>  
 
 
